ef6b4de44b4dc6d8ca6f33e4bbbac68bce2712f8
Jaekyun Seok <jaekyun@google.com>
2017-04-05 11:23:26 +0900

    YUREKA2: Enforce RRO on framework-res
    Overlays only for framework-res will be converted into RROs.
    
    Other overlays can't be converted due to some known issues on app RRO.
    
    Bug: 36231603
    Change-Id: Idca30fdbbcc990fd124de16a06a112a346612a61

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-#PRODUCT_ENFORCE_RRO_TARGETS := \
+PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
 # Boot animation
