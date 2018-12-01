96ad0b6573bfdfd56ddd594d047fea880cfafac6
Ethan Chen <intervigil@gmail.com>
2018-11-30 23:28:49 -0800

    YUREKA2:Add AOSP RCS packages

--- device.mk
+++ device.mk
@@ -360,6 +360,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
