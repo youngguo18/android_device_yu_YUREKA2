71dc11b145c357730b0016a46ae2a7a41d578248
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    Adding Audio HAL V5: Direction API

--- device.mk
+++ device.mk
@@ -102,8 +102,8 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     android.hardware.audio@2.0-service
 
