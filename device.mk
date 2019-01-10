729de27fffa431362acf9ff5800c7eb5419dc415
Bruno Martins <bgcngm@gmail.com>
2019-01-10 11:51:54 +0100

    YUREKA2:add XiaomiParts

--- device.mk
+++ device.mk
@@ -175,6 +175,10 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
