43f4f278b8437376af4dcd0d8b87ebcf13c3541f
jmjxperia <jmjxperia@users.noreply.github.com>
2019-02-20 11:12:20 +0530

    ril fix

--- device.mk
+++ device.mk
@@ -391,9 +391,6 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-rtti \
     libxml2
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.qti.pie.telephony=1
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
