ab37aabe5139d3a91c72b58def72dce95a5b22aa
BealeStBluesBoy <gustavo.cargnelutti@outlook.com>
2019-04-13 17:43:43 -0300

    YUREKA2:add mokee touch

--- device.mk
+++ device.mk
@@ -419,6 +419,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# MK hardware
+PRODUCT_PACKAGES += \
+    vendor.mokee.touch@1.0-service.xiaomi_8953
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
