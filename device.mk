ccace933d47370b6f069789cee147258e60f67a8
jmjxperia <jmjxperia@users.noreply.github.com>
2019-04-18 08:12:24 +0530

    YUREKA2:remove unwanted Display packages

--- device.mk
+++ device.mk
@@ -175,11 +175,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    android.hardware.configstore@1.1-service \
-    android.frameworks.displayservice@1.0_32 \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.0_vendor
+    android.hardware.memtrack@1.0-service
 
 # DRM
 PRODUCT_PACKAGES += \
