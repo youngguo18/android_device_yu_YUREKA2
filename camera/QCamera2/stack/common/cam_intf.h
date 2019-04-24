c006acc66aa2ba324205b6e549651065d578ff14
Yener <yener90@git.com>
2019-04-24 19:28:39 +0200

    QCamera2: Add subsampling info in reprocess param
    Issue: If CDS is enabled and if metadata is not present at
    the time of reprocess, CPP will not know the subsampling
    information and hence will result in image corruption.
    
    Fix: Instead of depending on metadata, add subsampling info
    to reprocess param itself.
    
    Change-Id: Iaedd19cd8585f600c14192fb504e07c341eb7a69

--- camera/QCamera2/stack/common/cam_intf.h
+++ camera/QCamera2/stack/common/cam_intf.h
@@ -452,6 +452,7 @@ typedef struct {
     /* opaque metadata required for reprocessing */
     int32_t private_data[MAX_METADATA_PRIVATE_PAYLOAD_SIZE_IN_BYTES];
     cam_rect_t crop_rect;
+    uint8_t is_uv_subsampled;
 } cam_reprocess_param;
 
 typedef struct {
@@ -567,7 +568,7 @@ typedef struct {
 } cam_stream_img_prop_t;
 
 typedef struct {
-    uint8_t enableStream; /*0 – stop and 1-start */
+    uint8_t enableStream; /*0 \96 stop and 1-start */
 } cam_request_frames;
 
 typedef struct {
