aa5acf03b08bf51e59c96b0a4d044dc63dd54038
jmjxperia <jmjxperia@users.noreply.github.com>
2019-05-13 16:53:42 +0530

    YUREKA2 : set SELINUX_IGNORE_NEVERALLOWS
    Sad ;_;

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -238,6 +238,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 ENABLE_VENDOR_IMAGE := true
