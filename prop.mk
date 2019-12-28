ff03e2d41f5a4611a25ce86836eb5e0e967f3f04
UtsavisGreat <utsavbalar1231@gmail.com>
2019-12-28 18:56:14 +0530

    YUREKA2: Optimize values and Switch lmkd to use enhance batch kill

--- prop.mk
+++ prop.mk
@@ -115,6 +115,20 @@ vendor.hw.fm.init=0
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.frp.pst=/dev/block/bootdevice/by-name/config
 
+# Set lmkd options
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.lmk.low=1001 \
+ro.lmk.medium=0 \
+ro.lmk.critical=800 \
+ro.lmk.critical_upgrade=false \
+ro.lmk.upgrade_pressure=100 \
+ro.lmk.downgrade_pressure=100 \
+ro.lmk.kill_heaviest_task=true \
+ro.lmk.kill_timeout_ms=15 \
+ro.lmk.use_minfree_levels=true \
+ro.lmk.enhance_batch_kill=true \
+ro.lmk.vmpressure_file_min=80640
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 av.debug.disable.pers.cache=1 \
