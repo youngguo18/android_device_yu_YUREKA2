ae77147c4252594a51896c5ec257a0539f0d3b7e
Anderson Mendes <anderson2000ano@gmail.com>
2020-01-24 19:22:01 -0300

    YUREKA2:add XiaomiDoze from msm8953-common

--- device.mk
+++ device.mk
@@ -176,6 +176,10 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
