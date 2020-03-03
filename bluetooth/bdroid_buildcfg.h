216e8963113964c90d90a1dbf7f043c5142b5745
youngguo18 <guoamao@163.com>
2020-03-03 20:58:08 +0800

    fix bt error: #define PROPERTY_VALUE_MAX  PROP_VALUE_MAX

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,7 +1,5 @@
-/**
- * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2016 The CyanogenMod Project
- * Copyright (C) 2018 The LineageOS Project
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,33 +16,22 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
+
 #define BTM_DEF_LOCAL_NAME   "Yureka 2"
-
-#include <cutils/properties.h>
-#include <string.h>
-
 #define MAX_ACL_CONNECTIONS    16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-
-#define BTM_WBS_INCLUDED TRUE
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 // Wide-band speech support
 #define BTIF_HF_WBS_PREFERRED TRUE
 
 // skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
-#define BT_CLEAN_TURN_ON_DISABLED 1
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
+
+/* Disable enhanced sco commands */
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
+
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS 12
 
-/* Enable HFP WBS feature */
-#define BTIF_HF_CLIENT_WBS_INCLUDED TRUE
-#undef PROPERTY_VALUE_MAX
-#endif
-
-/* Disable enhanced sco commands */
-#define BTM_SCO_ENHANCED_SYNC_DISABLED TRUE
+#endif /* _BDROID_BUILDCFG_H */
