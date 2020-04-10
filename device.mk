35c9ec2fbba5eab83b2563ebfdcfc9719db13fb6
Subinsmani <Subinsmani@users.noreply.github.com>
2020-04-10 00:36:01 +0000

    YUREKA2:: Adding Google AR Support
    Signed-off-by: Subinsmani <subins.mani@gmail.com>
    Signed-off-by: bablusss <baaswanthmadhav@gmail.com>
    Change-Id: I4e5b76a8665101e7c3c9e9f16e4ea7697dae3ece

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 
+# AR config
+PRODUCT_COPY_FILES += \
+    device/yu/YUREKA2/configs/android.hardware.camera.ar.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.ar.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
