1eeb7a1b39193db53e643c5d96a2dad861fc75cc
youngguo18 <guoamao@163.com>
2020-04-23 09:47:28 +0800

    YUREKA2:drop YUDoze

--- device.mk
+++ device.mk
@@ -140,10 +140,6 @@ PRODUCT_PACKAGES += \
     android.hardware.automotive.vehicle@2.0 \
     android.hardware.automotive.vehicle@2.0-manager-lib
 
-# Doze
-PRODUCT_PACKAGES += \
-    YUDoze
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     libldacBT_dec
