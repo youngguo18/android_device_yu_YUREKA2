71195fd54044523e794563875a07150a57f2c5c1
youngguo18 <guoamao@163.com>
2020-05-06 21:08:24 +0800

    YUREKA2:add missing android.hardware.camera.provider@2.5.so

--- device.mk
+++ device.mk
@@ -152,6 +152,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0 \
     android.hardware.camera.provider@2.4 \
+    android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.device@3.4
