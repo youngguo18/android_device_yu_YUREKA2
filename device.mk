ccc534ccf27e61c5a36876c7144e3d4e05865e0a
ZeeLog <Danjel.Zammit@gmail.com>
2020-05-24 16:27:52 +0200

    YUREKA2:hwc: switch to prebuilt hwc blobs

--- device.mk
+++ device.mk
@@ -163,7 +163,6 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_PACKAGES += \
     copybit.msm8953 \
     gralloc.msm8953 \
-    hwcomposer.msm8953 \
     memtrack.msm8953 \
     libqdMetaData.system \
     libdisplayconfig \
