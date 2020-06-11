4989f729de57b89cb6167e80eac83a873fbb9ba4
Ashay Kapadiya <kapadiyaashay@gmail.com>
2020-06-11 23:34:34 +0530

    YUREKA2:sos setup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bliss_YUREKA2.mk
+    $(LOCAL_DIR)/aosp_YUREKA2.mk
 
 COMMON_LUNCH_CHOICES := \
-    bliss_YUREKA2-userdebug
+    aosp_YUREKA2-userdebug
