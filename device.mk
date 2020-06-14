72df6cbe35f787128443abfc1e2cd3cc002ba8f2
sweetyicecare <gabrielseedev@outlook.com>
2020-06-14 10:13:36 +0000

    YUREKA2: add PA-overlay in path

--- device.mk
+++ device.mk
@@ -17,8 +17,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-pa
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
