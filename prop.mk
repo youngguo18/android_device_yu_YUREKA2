fc6b4696513b1d044d606b07abf2e99d5f1b9816
youngguo18 <guoamao@163.com>
2020-07-12 09:16:29 +0800

    revert YUREKA2:Enable Codec2

--- prop.mk
+++ prop.mk
@@ -155,7 +155,6 @@ persist.vendor.ims.disableQXDMLogs=1
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 av.debug.disable.pers.cache=1 \
-debug.media.codec2=2 \
 debug.stagefright.omx_default_rank.sw-audio=1 \
 debug.stagefright.omx_default_rank=0 \
 media.msm8956hw=0 \
