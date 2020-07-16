0ab411d2c1b1abc88b090b97dd04e7dfad8c6991
jmjxperia <jathinjater1996@gmail.com>
2020-07-16 19:51:32 +0530

    remove rotator_downscale prop

--- prop.mk
+++ prop.mk
@@ -102,7 +102,6 @@ sdm.debug.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=1 \
 vendor.display.disable_skip_validate=1 \
 vendor.gralloc.enable_fb_ubwc=1
-sdm.debug.rotator_downscale=1 \
 
 # DPM
 PRODUCT_PROPERTY_OVERRIDES += \
