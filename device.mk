8156d40cf52eebd542d7b76632b217909ca2b255
ShihabZzz <Shihab.riadn@gmail.com>
2020-09-12 21:40:41 +0600

    YUREKA2:Build additional camera packages

--- device.mk
+++ device.mk
@@ -160,6 +160,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
     android.hardware.camera.device@3.4
 
 # Dex
