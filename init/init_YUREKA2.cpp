2ada28f2a104631a4e2f676372b6b6b0d7905028
Lucchetto <lucchetto.tie@live.com>
2020-09-16 18:48:36 +0200

    YUREKA2:adapt libinit to Android 11

--- init/init_YUREKA2.cpp
+++ init/init_YUREKA2.cpp
@@ -51,7 +51,10 @@ char const *drop_shadow_cache_size;
 
 using android::base::GetProperty;
 using android::base::ReadFileToString;
-using android::init::property_set;
+
+int property_set(const char *key, const char *value) {
+    return __system_property_set(key, value);
+}
 
 void property_override(char const prop[], char const value[])
 {
