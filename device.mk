61dbaec627f66bdf3e0808520ff4bdace4f2dffb
youngguo18 <guoamao@163.com>
2020-09-19 11:19:54 +0800

    YUREKA2:remove Gcam

--- device.mk
+++ device.mk
@@ -166,7 +166,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
-    android.hardware.camera.device@3.4
+    android.hardware.camera.device@3.4 \
+    Snap
 
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
@@ -470,10 +471,6 @@ PRODUCT_BOOT_JARS += \
     WfdCommon
 PRODUCT_PACKAGES += \
     libaacwrapper	
-
-#Prebuilt Apps
-PRODUCT_PACKAGES += \
-    GCam
 
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/mido/mido-vendor.mk)
