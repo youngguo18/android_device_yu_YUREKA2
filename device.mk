76be58a72be235cb639429754be7859c74db7dca
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    YUREKA2:Build new aidl qti power HAL

--- device.mk
+++ device.mk
@@ -354,7 +354,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml \
